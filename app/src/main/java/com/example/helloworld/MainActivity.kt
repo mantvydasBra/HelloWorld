35a80549a923c14d1b6a6a308a83c2b33907dd8d
Mantvydas Bražėnas <mantvydasbr7@gmail.com>
2021-09-28 22:54:04 +0300

    Revert "Added a button, which changes color of text"
    This reverts commit 40575bcfaeac82f51cbd596769fbc44233fa25b5.

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.helloworld
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -14,10 +13,5 @@ class MainActivity : AppCompatActivity() {
     fun onBtnClickChangeText(view: android.view.View) {
         val textView = findViewById<TextView>(R.id.tvMain)
         textView.setText("Hello world my first code in kotlin!").toString()
-    }
-
-    fun onBtnClickChangeColor(view: android.view.View) {
-        val textView = findViewById<TextView>(R.id.tvMain)
-        textView.setTextColor(Color.parseColor("#ff0000"))
     }
 }
