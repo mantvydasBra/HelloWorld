74f085758704bc57a7fc6507e8ada4444e1a485b
Mantvydas Bražėnas <mantvydasbr7@gmail.com>
2021-09-28 22:29:33 +0300

    Added a button, which prints text

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onBtnClickChangeText(view: android.view.View) {
+        val textView = findViewById<TextView>(R.id.tvMain)
+        textView.setText("Hello world my first code in kotlin!").toString()
     }
 }
